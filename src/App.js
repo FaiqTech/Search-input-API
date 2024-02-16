898eceadca85d1619cecbe94bcdbfa30290b33e5
FaiqTech <hesenovfaiq@icloud.com>
2024-02-16 12:06:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const Base_Url = "https://jsonplaceholder.typicode.com/users";
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(Base_Url);
+      setPosts(response.data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const filteredPosts = posts.filter((post) =>
+    post.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        className="input"
+        type="text"
+        placeholder="Search..."
+        onChange={handleChange}
+        value={searchInput}
+      />
+      {filteredPosts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
+    </>
   );
 }
 
